ed8561add6677da1e6f747a67d3d7e42e9123d48
Rahat <rrahatislamm@gmail.com>
2026-06-07 23:47:31 +0530

    ...

--- Java/Example.java
+++ Java/Example.java
@@ -1,9 +1,11 @@
-import PackageExamples.Add;
+import mypackage.Add;
 
 public class Example {
     public static void main(String[] args) {
         Add a = new Add();
         int sum = a.add(2, 5);
         System.out.println(sum);
+        int minus = a.minus(10, 5);
+        System.out.println(minus);
     }
 }
